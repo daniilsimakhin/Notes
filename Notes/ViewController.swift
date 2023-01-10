23f2974c0f64d7347055ce67fe4259fa57071a2a
daniilSimakhin <danil.110@yandex.ru>
2023-01-10 19:37:47 +0700

    Init

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Даниил Симахин on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
