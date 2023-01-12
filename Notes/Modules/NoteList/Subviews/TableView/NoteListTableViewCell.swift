9e41d957f71c273646fd152ac854209ab63640ed
daniilSimakhin <danil.110@yandex.ru>
2023-01-12 12:54:11 +0700

    Create table view cell

---
+++ Notes/Modules/NoteList/Subviews/TableView/NoteListTableViewCell.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class NoteListTableViewCell: UITableViewCell {
+    enum Constraints {
+        static let offset: CGFloat = 10
+    }
+    
+    private lazy var noteLabel: UILabel = {
+        let label = UILabel()
+        label.adjustsFontForContentSizeCategory = true
+        label.font = .preferredFont(forTextStyle: .body)
+        label.numberOfLines = 1
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        applyAppearance()
+        applyConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension NoteListTableViewCell {
+    func applyAppearance() {
+        contentView.addSubview(noteLabel)
+    }
+    
+    func applyConstraints() {
+        NSLayoutConstraint.activate([
+            noteLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constraints.offset),
+            noteLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Constraints.offset),
+            noteLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Constraints.offset),
+            noteLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constraints.offset),
+        ])
+    }
+}
+
+extension NoteListTableViewCell {
+    func configure(note: Note) {
+        noteLabel.text = note.text
+    }
+}
