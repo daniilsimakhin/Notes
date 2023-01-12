2a3e9163d70419868f0f0f5733f2a0a01e18f319
daniilSimakhin <danil.110@yandex.ru>
2023-01-12 14:28:26 +0700

    Add NotesStore, nav bar button

--- Notes/Modules/NoteList/NoteListViewController.swift
+++ Notes/Modules/NoteList/NoteListViewController.swift
@@ -2,8 +2,11 @@ import UIKit
 
 class NoteListViewController: UIViewController {
     
-    var notes = [Note(id: "0", text: "Hello1"), Note(id: "1", text: "Hello2"), Note(id: "2", text: "Hello3")]
-    lazy var dataSource = NoteListDataSource(notes: notes)
+    var store = NotesStore()
+    lazy var dataSource = NoteListDataSource(notes: store.notes)
+    var baseView: NoteListView {
+        return view as! NoteListView
+    }
     
     override func loadView() {
         let noteListView = NoteListView(delegate: self, dataSource: self)
@@ -13,6 +16,8 @@ class NoteListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         applyAppearance()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNoteTapped))
     }
 }
 
@@ -20,9 +25,31 @@ private extension NoteListViewController {
     func applyAppearance() {
         title = C.Strings.notes
     }
+    
+    @objc func addNoteTapped(_ sender: UIButton!) {
+        
+        let alert = UIAlertController(title: "Add note", message: "Enter a text", preferredStyle: .alert)
+
+        alert.addTextField { (textField) in
+            textField.placeholder = "Some default text"
+        }
+
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { [weak alert] (_) in
+            guard let text = alert?.textFields?[0].text else { return }
+            self.store.append(text: text)
+            self.dataSource.applySnapshot(notes: self.store.notes)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 }
 
 extension NoteListViewController: NoteListViewDataSource {
+    var notes: [Note] {
+        return store.notes
+    }
+    
     var noteListDataSource: NoteListDataSource {
         return dataSource
     }
@@ -30,7 +57,8 @@ extension NoteListViewController: NoteListViewDataSource {
 
 extension NoteListViewController: NoteListViewDelegate {
     func delete(with indexPath: IndexPath) {
-        notes.remove(at: indexPath.row)
+        store.remove(note: notes[indexPath.row])
+        dataSource.applySnapshot(notes: notes)
     }
     
     func pressedCell(with indexPath: IndexPath) {
