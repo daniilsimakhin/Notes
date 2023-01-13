4cfc866ed78aa884652e21147ab5f63c4391385c
daniilSimakhin <danil.110@yandex.ru>
2023-01-13 13:59:17 +0700

    Refactor NotesStore

--- Notes/Services/Store/NotesStore.swift
+++ Notes/Services/Store/NotesStore.swift
@@ -2,7 +2,8 @@ import Foundation
 import CoreData
 import UIKit
 
-struct NotesStore {
+class NotesStore {
+    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     var notes: [Note]
@@ -12,11 +13,11 @@ struct NotesStore {
         loadNotes()
     }
     
-    mutating func append(text: String) {
+    func append(text: String) {
         let note = Note(context: context)
         note.text = text
         note.id = UUID().uuidString
-        
+        note.dateCreating = Date().timeIntervalSince1970
         while notes.contains(where: { $0.id == note.id }) {
             note.id = UUID().uuidString
         }
@@ -24,16 +25,27 @@ struct NotesStore {
         saveNotes()
     }
     
-    mutating func remove(note: Note) {
+    func remove(_ note: Note) {
         guard let index = notes.firstIndex(of: note) else { return }
         context.delete(notes[index])
         notes.remove(at: index)
         saveNotes()
     }
     
-    func getNote(index: Int) -> Note? {
-        guard index < notes.count else { return nil }
-        return notes[index]
+    func replaceNote(_ note: Note) {
+        guard notes.contains(where: { $0.id == note.id }) else { return }
+        for i in notes.indices {
+            if notes[i].id == note.id {
+                let newNote = Note(context: context)
+                newNote.id = note.id
+                newNote.text = note.text
+                newNote.dateCreating = Date().timeIntervalSince1970
+                remove(note)
+                notes.insert(newNote, at: i)
+                saveNotes()
+                return
+            }
+        }
     }
     
     func saveNotes() {
@@ -44,10 +56,13 @@ struct NotesStore {
         }
     }
     
-    mutating func loadNotes() {
+    func loadNotes() {
         let request: NSFetchRequest<Note> = Note.fetchRequest()
         do {
-            notes = try context.fetch(request)
+            let data = try context.fetch(request)
+            notes = data.sorted(by: { note1, note2 in
+                note1.dateCreating < note2.dateCreating
+            })
         } catch {
             print("Error with load notes \(error.localizedDescription)")
         }
